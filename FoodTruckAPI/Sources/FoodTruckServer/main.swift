e13aae8292b4714f62e05c51951ec99e459a4c73
Alex Nguyen <alex.nguyen@td.com>
2017-04-11 23:09:44 -0400

    Added SSL to Kitura

--- FoodTruckAPI/Sources/FoodTruckServer/main.swift
+++ FoodTruckAPI/Sources/FoodTruckServer/main.swift
@@ -12,7 +12,16 @@ trucks = FoodTruck()
 
 let controller = FoodTruckController(backend: trucks)
 
-let port = Int(8080)
-Log.verbose("Assigned port \(port)")
-Kitura.addHTTPServer(onPort: port, with: controller.router)
-Kitura.run()
+#if os(Linux)
+    let myCertPath = "/creds/Self-Signed/cert.pem"
+    let myKeyPath = "/creds/Self-Signed/key.pem"
+    
+    let mySSLConfig =  SSLConfig(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath, usingSelfSignedCerts: true)
+    mySSLConfig.cipherSuite = "ALL"
+    Kitura.addHTTPServer(onPort: 8080, with: controller.router, withSSL: mySSLConfig)
+#else
+    let port = Int(8080)
+    Log.verbose("Assigned port \(port)")
+    Kitura.addHTTPServer(onPort: port, with: controller.router)
+    Kitura.run()
+#endif
